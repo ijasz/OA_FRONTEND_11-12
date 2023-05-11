130b9267482a1a0196f35747ab86b3fabf081ff0
ijasz <jass07rtr@gmail.com>
2023-05-11 11:16:30 +0530

    abou execution context and call stack

---
+++ JS/types-of-functions/index.js
@@ -0,0 +1,30 @@
+var a = "ocean";
+var b = "ajeeth";
+
+greeting(a);
+
+// function statement
+
+function greeting(name) {
+  console.log("hello" + " " + name);
+}
+
+greeting(b);
+// console.log("start");
+
+// greeting("ocean");
+
+// console.log("end");
+
+// greeting("ajeeth");
+
+// function expression
+//function acts as a value
+
+// what is the difference between function statement vs function expression
+
+// let a = function greeting(a) {
+//   console.log("hello" + " " + a);
+// };
+
+// a("ocean");
