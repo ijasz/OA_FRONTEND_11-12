76b02467e3e9d7a8ddb0d0e5eb3e088b4fe90544
ijasz <jass07rtr@gmail.com>
2023-05-23 11:26:06 +0530

    Web Api

---
+++ JS/Web_Api/index.js
@@ -0,0 +1,49 @@
+// ----------------------------------------------------
+// Local Storage(Web Api)
+// ----------------------------------------------------
+
+// How to set data(key:value) in local Storage?
+// localStorage.setItem("name1", "monica");
+// localStorage.setItem("name2", "monica");
+// localStorage.setItem("name3", "monica");
+// localStorage.setItem("name4", "monica");
+// localStorage.setItem("name5", "monica");
+// localStorage.setItem("name6", "monica");
+// localStorage.setItem("name7", "monica");
+// localStorage.setItem("name8", "monica");
+// localStorage.setItem("name9", "monica");
+// localStorage.setItem("name10", "monica");
+
+// How to get data(key) in local Storage?
+// console.log(localStorage.getItem("name"));
+
+// How to delete data(key) in local Storage?
+// localStorage.removeItem("name");
+
+// How to remove all the localStorage items?
+// localStorage.clear();
+
+// ----------------------------------------------------
+// Session Storage(Web Api)
+// ----------------------------------------------------
+
+// How to set data(key:value) in session Storage?
+// sessionStorage.setItem("name1", "monica");
+// sessionStorage.setItem("name2", "monica");
+// sessionStorage.setItem("name3", "monica");
+// sessionStorage.setItem("name4", "monica");
+// sessionStorage.setItem("name5", "monica");
+// sessionStorage.setItem("name6", "monica");
+// sessionStorage.setItem("name7", "monica");
+// sessionStorage.setItem("name8", "monica");
+// sessionStorage.setItem("name9", "monica");
+// sessionStorage.setItem("name10", "monica");
+
+// How to get data(key) in sessionStorage?
+// console.log(sessionStorage.getItem("name"));
+
+// How to delete data(key) in session Storage?
+// sessionStorage.removeItem("name");
+
+// How to remove all the sessionStorage items?
+// sessionStorage.clear();
